52bf4a4d9599751fe5d6b12e301ca19af468fe10
Giorgio Gamberoni <giorgio.gamberoni@musement.com>
2019-11-12 12:18:11 +0100

    add active class

--- src/components/nav-bar/nav-bar.tsx
+++ src/components/nav-bar/nav-bar.tsx
@@ -8,10 +8,6 @@ import { Component, h, getAssetPath } from '@stencil/core';
 })
 export class NavBar {
 
-  changeLanguage (lang: string) {
-    console.log(lang)
-  }
-
   render() {
     return (
       <nav>
@@ -19,16 +15,12 @@ export class NavBar {
           <img src={getAssetPath('./nav-bar-assets/brand.png')} alt="sfs"/>
         </a>
         <div class="links">
-          <stencil-route-link url="/">
-            'USER_LIST'
+          <stencil-route-link url="/" activeClass="active" exact={true}>
+            User List
           </stencil-route-link>
-          <stencil-route-link url="/posts">
-            'POST_LIST'
+          <stencil-route-link url="/posts" activeClass="active">
+            Post List
           </stencil-route-link>
-        </div>
-        <div>
-          <button type="button" onClick={() => this.changeLanguage('it')}>'BUTTON_LANG_IT')</button>
-          <button type="button" onClick={() => this.changeLanguage('en')}>'BUTTON_LANG_EN'</button>
         </div>
       </nav>
     );
